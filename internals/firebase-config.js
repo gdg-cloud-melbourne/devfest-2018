3552e6bf4a9a9f674fa11f6287ca6a5adcd384b0
Alex <alexzhang1931@gmail.com>
2018-08-17 15:12:49 +1000

    add firestore cloud build file; change firestore config

--- internals/firebase-config.js
+++ internals/firebase-config.js
@@ -1,12 +1,13 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccount';
+// import serviceAccount from '../serviceAccount';
 
 let firestore = null;
 export function initializeFirebase() {
   return new Promise((resolve) => {
 
     const firebaseApp = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.applicationDefault(),
+      databaseURL: 'https://gdgcloud-devfest.firebaseio.com'
     });
     firestore = admin.firestore();
     resolve(firebaseApp);
