ca04e773de4e0bf78d3e67f4ec7f1340ea89dcaa
Alex <alexzhang1931@gmail.com>
2018-08-21 11:32:33 +1000

    change firebase-admin auth to use env variable

--- internals/firebase-config.js
+++ internals/firebase-config.js
@@ -1,12 +1,17 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccount';
 
 let firestore = null;
 export function initializeFirebase() {
   return new Promise((resolve) => {
+    let privatekey = JSON.parse(JSON.stringify(process.env.AUTH_KEY).split('\\\\').join('\\'));
 
     const firebaseApp = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: 'firebase-adminsdk-aymeb@gdgcloud-devfest.iam.gserviceaccount.com',
+        privateKey: privatekey
+      }),
+      databaseURL: 'https://gdgcloud-devfest.firebaseio.com'
     });
     firestore = admin.firestore();
     resolve(firebaseApp);
