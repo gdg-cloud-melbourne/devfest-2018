3a4c2da5ca1c1ad398e432a906b2c2fe01745231
James Percy <james.percy@papercut.com>
2018-08-28 21:05:40 +1000

    added service worker unregistration

--- scripts/service-worker-registration.js
+++ scripts/service-worker-registration.js
@@ -1,13 +1,13 @@
-// (function () {
-//   'use strict';
-//   const unregister = () => {
-//     const URL = 'service-worker.js';
-//     if ('serviceWorker' in navigator) {
-//       navigator.serviceWorker.getRegistration(URL)
-//         .then((registration) => {
-//           registration.unregister();
-//         });
-//     }
-//   };
-//   window.addEventListener('load', unregister);
-// })();
+(function () {
+  'use strict';
+  const unregister = () => {
+    const URL = 'service-worker.js';
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistration(URL)
+        .then((registration) => {
+          registration.unregister();
+        });
+    }
+  };
+  window.addEventListener('load', unregister);
+})();
